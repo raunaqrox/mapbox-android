ce13628109d2f7d380208057b6be8981f9562b0b
raunaqrox <sahebjot94@gmail.com>
2016-04-10 18:36:31 +0530

    socket io added just not connecting yet

--- app/src/main/java/promignis/com/mapbox/MainActivity.java
+++ app/src/main/java/promignis/com/mapbox/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.PersistableBundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
@@ -20,10 +21,19 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
+import java.net.URISyntaxException;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+import promignis.com.mapbox.Utils.URLStore;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private MapView mapView;
     private Location currentBestLocation;
+    private Socket mSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,17 +43,22 @@ public class MainActivity extends AppCompatActivity {
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.onCreate(savedInstanceState);
 
+        setupMap();
+        setupSocketIO();
+    }
+
+    private void setupMap() {
         mapView.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
 //                mapboxMap.setStyleUrl(Style.DARK);
                 currentBestLocation = getLastBestLocation();
-                if(currentBestLocation != null) {
+                if (currentBestLocation != null) {
                     LatLng currLatLng = new LatLng(currentBestLocation.getLatitude(), currentBestLocation.getLongitude());
                     mapboxMap.addMarker(new MarkerOptions()
-                    .position(currLatLng)
-                    .title("MY CURRENT LOCATION")
-                    .snippet("WELCOME TO MY MARKER"));
+                            .position(currLatLng)
+                            .title("MY CURRENT LOCATION")
+                            .snippet("WELCOME TO MY MARKER"));
 
                     CameraPosition position = new CameraPosition.Builder()
                             .target(currLatLng)
@@ -52,17 +67,40 @@ public class MainActivity extends AppCompatActivity {
                             .tilt(30)
                             .build();
                     mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 2000);
+                } else {
+                    Debug("currentBestLocation null");
                 }
             }
         });
     }
+
+    private void setupSocketIO() {
+        try {
+            mSocket = IO.socket(URLStore.BASE_URL);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        setupSocketEvents();
+    }
+
+    private void setupSocketEvents() {
+        mSocket.on(Socket.EVENT_CONNECT, Connected);
+    }
+
+    private Emitter.Listener Connected = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            Log.d("SOCKET.IO", "CONNECTED");
+            Debug("CONNECTED");
+        }
+    };
 
     private Location getLastBestLocation() {
         LocationManager locationManager = (LocationManager)
                 getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(MainActivity.this, "TURN ON GPS", Toast.LENGTH_SHORT).show();
+            Debug("Give gps permissions!");
             return null;
         }
         Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
@@ -84,6 +122,11 @@ public class MainActivity extends AppCompatActivity {
             return locationNet;
         }
     }
+
+    private void Debug(String msg) {
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+    }
+
 
     @Override
     protected void onResume() {
---
+++ app/src/main/java/promignis/com/mapbox/Utils/URLStore.java
@@ -0,0 +1,8 @@
+package promignis.com.mapbox.Utils;
+
+/**
+ * Created by sahebjot on 4/10/16.
+ */
+public class URLStore {
+    public static final String BASE_URL  = "https://minimalist-backend.herokuapp.com";
+}
